3c197209ad066dbc0840f35db6af73cf32eb27f4
Andy Tuba <andytuba@andytuba.com>
2015-03-14 13:16:28 -0700

    use GM_openInTab to avoid security restrictions on window.open

--- greasemonkey/userscript.js
+++ greasemonkey/userscript.js
@@ -5,19 +5,18 @@
 // @description  open a bunch of pix from tmbo at once
 // @author       andytuba
 // @match        https://thismight.be/offensive/?c=hof&t=today
-// @grant        none
+// @grant        GM_openInTab
 // ==/UserScript==
 
 setTimeout(function() {
 	var allLinks = document.querySelectorAll('#grid-container a[href^="/offensive/pages/pic.php"]');
-	var selection = [].slice.call(allLinks, 0, 20);
+	var selection = [].slice.call(allLinks, 0, 1, 20);
 	var urls = selection.map(function(elem, index) {
 		return elem.href; 
 	});
     
     urls.forEach(function(url, index) {
-		setTimeout(window.open, index * 200, url); 
+		setTimeout(GM_openInTab, index * 200, url); 
 	});
 }, 100);
-
 
